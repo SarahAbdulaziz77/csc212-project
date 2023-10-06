116c3c63d098b3aebeb1f7b9c8e1b225c7f7cee1
MashaelKQ <117913825+MashaelKQ@users.noreply.github.com>
2023-10-06 17:58:43 +0300

    Update GITHUB.java

--- csc212PA/src/GITHUB.java
+++ csc212PA/src/GITHUB.java
@@ -2,8 +2,8 @@ public class GITHUB {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("test");
-		System.out.println("test2");
+		System.out.println("hello");
+		System.out.println("there");
 
 	}
 
