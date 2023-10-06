1dbafb5ecc4ffa221d688d62cd37af86b269a1fa
FutunS <147118986+FutunS@users.noreply.github.com>
2023-10-07 00:18:45 +0300

    Update Node.java

--- csc212PA/src/Node.java
+++ csc212PA/src/Node.java
@@ -1,4 +1,18 @@
 
-public class Node {
+public class Node<T> {
+	
 
-}
+
+		private T data ; 
+		private Node<T> next ; 
+
+		public Node(T obj)
+		{
+		data = obj ; 
+		next = null ; 
+		}
+
+	
+
+		    
+		}
