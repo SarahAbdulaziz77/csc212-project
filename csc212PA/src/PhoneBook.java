7a23f97c0c6d4b6dd8aef98e8e2dfa07e63398eb
MashaelKQ <117913825+MashaelKQ@users.noreply.github.com>
2023-10-07 10:49:51 +0300

    Update PhoneBook.java

--- csc212PA/src/PhoneBook.java
+++ csc212PA/src/PhoneBook.java
@@ -1,5 +1,13 @@
 package CSC212_PA;
 
 public class PhoneBook {
-
+//phone book methods 
+	
+	
+//main method
+	public static void main(String[] args) {
+	Contact user1 = new Contact("name", "012", "gmail","house", "2003", "extranotes");
+	
+System.out.print(user1);
+	}
 }
