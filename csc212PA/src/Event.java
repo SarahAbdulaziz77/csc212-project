56f8cd51601a8b1b02ba4e7b0a08011986339118
MashaelKQ <117913825+MashaelKQ@users.noreply.github.com>
2023-10-08 15:25:19 +0300

    Update Event.java

--- csc212PA/src/Event.java
+++ csc212PA/src/Event.java
@@ -1,5 +1,9 @@
 package CSC212_PA;
 
 public class Event {
-
+	public String title;
+	public String time;
+	public String location;
+	LinkedList<Contact> ContactsList = new LinkedList<Contact>();
+	
 }
