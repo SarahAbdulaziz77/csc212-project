93e193acc3da6dbef509c8943d4f8d0380600d44
FutunS <147118986+FutunS@users.noreply.github.com>
2023-10-10 06:52:15 +0300

    Update Event.java

--- csc212PA/src/Event.java
+++ csc212PA/src/Event.java
@@ -4,6 +4,7 @@ public class Event {
 	public String title;
 	public String time;
 	public String location;
+	public String contactName;
 	LinkedList<Contact> ContactsList = new LinkedList<Contact>();
 	
 }
