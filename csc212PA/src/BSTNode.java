bdeb2065657db85468b78e885136a151a7a3c6cc
MashaelKQ <117913825+MashaelKQ@users.noreply.github.com>
2023-11-28 00:40:04 +0300

    Update BSTNode.java

--- csc212PA/src/BSTNode.java
+++ csc212PA/src/BSTNode.java
@@ -1,14 +1,14 @@
- public class BSTNode {
+public class BSTNode<T> {
     public String key;
-    public Contact data;
-    public BSTNode left, right;
+    public T data;
+    public BSTNode<T> left, right;
     /** Creates a new instance of BSTNode */
-    public BSTNode(String k, Contact val) {
+    public BSTNode(String k, T val) {
         key = k;
         data = val;
         left = right = null;
     }
-    public BSTNode(String k, Contact val, BSTNode l, BSTNode r) {
+    public BSTNode(String k, T val, BSTNode<T> l, BSTNode<T> r) {
         key = k;
         data = val;
         left = l;
