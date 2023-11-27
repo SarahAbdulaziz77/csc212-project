1f3076bffd6b2822b9960b9cd35bd98cbcaa4b5e
SarahAbdulaziz77 <114563723+SarahAbdulaziz77@users.noreply.github.com>
2023-11-27 03:19:25 +0300

    Update BSTNode.java

--- csc212PA/src/BSTNode.java
+++ csc212PA/src/BSTNode.java
@@ -1,16 +1,17 @@
-public class BSTNode <T> {
-    public int key;
-    public T data;
-    public BSTNode<T> left, right;
-
-    /**
-     * Creates a new instance of BSTNode
-     */
-    public BSTNode(int k, T val) {
+ public class BSTNode {
+    public String key;
+    public Contact data;
+    public BSTNode left, right;
+    /** Creates a new instance of BSTNode */
+    public BSTNode(String k, Contact val) {
         key = k;
         data = val;
         left = right = null;
     }
-
-
+    public BSTNode(String k, Contact val, BSTNode l, BSTNode r) {
+        key = k;
+        data = val;
+        left = l;
+        right = r;
+    }
 }
